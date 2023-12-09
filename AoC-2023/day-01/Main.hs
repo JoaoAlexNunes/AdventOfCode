52aae3bf7c0108a8a81eef51d9b6a420154982aa
JoaoAlexNunes <jalnunes.2002@gmail.com>
2023-12-09 03:04:25 +0100

    Day 1 done

---
+++ AoC-2023/day-01/Main.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE ApplicativeDo #-}
+
+module Main where
+
+import Control.Applicative (asum)
+import Control.Arrow ((&&&))
+import Data.Char (digitToInt, intToDigit, isDigit)
+import Data.List (isPrefixOf, tails)
+import Data.Maybe (mapMaybe)
+
+type Input = [String]
+
+calibrationValue :: [Int] -> Int
+calibrationValue = (+) <$> ((* 10) . head) <*> last
+
+guardedBy :: (a -> b) -> (a -> Bool) -> a -> Maybe b
+guardedBy f p x
+  | p x = Just $ f x
+  | otherwise = Nothing
+
+solve :: (b -> Maybe Int) -> (a -> [b]) -> [a] -> Int
+solve f g = sum . map (calibrationValue . mapMaybe f . g)
+
+part1 :: Input -> Int
+part1 = solve (digitToInt `guardedBy` isDigit) id
+
+digits :: [(String, Int)]
+digits =
+  [ ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9)
+  ]
+    <> [(pure (intToDigit x), x) | x <- [1 .. 9]]
+
+part2 :: Input -> Int
+part2 = solve decodeDigit tails
+  where
+    decodeDigit :: String -> Maybe Int
+    decodeDigit s = asum . map find $ digits
+      where
+        find (k, v)
+          | k `isPrefixOf` s = Just v
+          | otherwise = Nothing
+
+prepare :: String -> Input
+prepare = lines
+
+main :: IO ()
+main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
