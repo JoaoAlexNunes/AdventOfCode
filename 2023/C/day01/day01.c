db8cc1872d44e04e26f9ff78791dda76d0c2e7d3
JoaoAlexNunes <jalnunes.2002@gmail.com>
2023-12-20 09:25:29 +0100

    C: day01 done

--- 2023/C/day01/day01.c
+++ 2023/C/day01/day01.c
@@ -4,26 +4,42 @@
 #include <string.h>
 #include <assert.h>
 
+// Defines
 #define TEST_FILE "testinput.txt"
 #define INPUT_FILE "input.txt"
+#define NUMBERS 9
+#define MAX_LENGTH 5
 
 // Function prototypes
 int part1(char *contents);
 int part2(char *contents);
 char *readFile(char *filename);
+int isInNumbers(char *word, int length);
+int isWordNumeric(char *buffer, size_t buffer_index);
 
-// Part 1
+// Structures
 typedef struct
 {
     char num;
     int isInitialized;
 } Num;
 
+char *numbers[NUMBERS][2] = {
+    {"one", "1"},
+    {"two", "2"},
+    {"three", "3"},
+    {"four", "4"},
+    {"five", "5"},
+    {"six", "6"},
+    {"seven", "7"},
+    {"eight", "8"},
+    {"nine", "9"},
+};
+
+// Funtions
 int main()
 {
-    char *contents = readFile(TEST_FILE);
-    // char *contents = readFile(INPUT_FILE);
-    //  printf("%s\n", contents);
+    char *contents = readFile(INPUT_FILE);
 
     // Calculate results for Part One and Part Two
     int resultPart1 = part1(contents);
@@ -36,6 +52,19 @@ int main()
     free(contents);
 
     return 0;
+}
+
+// Implementation for prepare function
+char *readFile(char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    fseek(file, 0, SEEK_END);
+    int length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char *contents = malloc(sizeof(char) * length + 1);
+    fread(contents, 1, length, file);
+    contents[length] = '\0';
+    return contents;
 }
 
 // Implementation for part1 function
@@ -64,7 +93,6 @@ int part1(char *contents)
 
         if (contents[i] == '\n')
         {
-            printf("%d\n", first.num);
             total += (first.num * 10) + last.num;
             first.isInitialized = 0;
             last.isInitialized = 0;
@@ -76,19 +104,74 @@ int part1(char *contents)
 // Implementation for part2 function
 int part2(char *contents)
 {
-    // Your implementation here...
+    Num first = {
+        .isInitialized = 0,
+    };
+    Num last = {
+        .isInitialized = 0,
+    };
+
+    int total = 0;
+
+    for (size_t i = 0; contents[i] != '\0'; i++)
+    {
+        if (isalpha(contents[i]))
+        {
+            int value = isWordNumeric(contents, i);
+            if (value != 0)
+            {
+                if (first.isInitialized == 0)
+                {
+                    first.isInitialized = 1;
+                    first.num = (value + '0') - '0';
+                }
+                last.num = (value + '0') - '0';
+            }
+        }
+        if (isdigit(contents[i]))
+        {
+            if (first.isInitialized == 0)
+            {
+                first.isInitialized = 1;
+                first.num = contents[i] - '0';
+            }
+            last.num = contents[i] - '0';
+        }
+        if (contents[i] == '\n')
+        {
+            total += (first.num * 10) + last.num;
+            first.isInitialized = 0;
+            last.isInitialized = 0;
+        }
+    }
+    return total;
+}
+
+int isInNumbers(char *word, int length)
+{
+    word[length] = '\0';
+    for (size_t i = 0; i < NUMBERS; i++)
+    {
+        if (strcmp(numbers[i][0], word) == 0)
+        {
+            return atoi(numbers[i][1]);
+        }
+    }
     return 0;
 }
 
-// Implementation for prepare function
-char *readFile(char *filename)
+int isWordNumeric(char *buffer, size_t buffer_index)
 {
-    FILE *file = fopen(filename, "r");
-    fseek(file, 0, SEEK_END);
-    int length = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    char *contents = malloc(sizeof(char) * length + 1);
-    fread(contents, 1, length, file);
-    contents[length] = '\0';
-    return contents;
+    char *num_word = malloc(sizeof(char) * MAX_LENGTH);
+    for (size_t i = 0; i < MAX_LENGTH; i++)
+    {
+        num_word[i] = buffer[buffer_index];
+        buffer_index++;
+        int value = isInNumbers(num_word, i + 1);
+        if (value != 0)
+        {
+            return value;
+        }
+    }
+    return 0;
 }
