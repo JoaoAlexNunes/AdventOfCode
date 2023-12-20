220959ec48c40f6aa28ed5c11728b629cecbef4e
JoaoNunes <jalnunes.2002@gmail.com>
2023-12-20 08:14:38 +0100

    Update day01.c
    Part 1 done

--- 2023/C/day01/day01.c
+++ 2023/C/day01/day01.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <assert.h>
 
 #define TEST_FILE "testinput.txt"
 #define INPUT_FILE "input.txt"
 
 // Function prototypes
-int part1(char* contents);
-int part2(char* contents);
+int part1(char *contents);
+int part2(char *contents);
 char *read_from_file(char *filename);
 
-int main() {
+// Part 1
+typedef struct
+{
+    char num;
+    int is_initted;
+} Num;
+
+int main()
+{
     char *contents = read_from_file(TEST_FILE);
     // char *contents = read_from_file(INPUT_FILE);
-    // printf("%s\n", contents);
+    //  printf("%s\n", contents);
 
     // Calculate results for Part One and Part Two
     int resultPart1 = part1(contents);
@@ -25,19 +36,50 @@ int main() {
 }
 
 // Implementation for part1 function
-int part1(char* contents) {
-    // Your implementation here...
-    return 0;
+int part1(char *contents)
+{
+    Num first = {
+        .is_initted = 0,
+    };
+    Num last = {
+        .is_initted = 0,
+    };
+
+    int total = 0;
+
+    for (size_t i = 0; contents[i] != '\0'; i++)
+    {
+        if (isdigit(contents[i]))
+        {
+            if (first.is_initted == 0)
+            {
+                first.is_initted = 1;
+                first.num = contents[i] - '0';
+            }
+            last.num = contents[i] - '0';
+        }
+
+        if (contents[i] == '\n')
+        {
+            printf("%d\n", first.num);
+            total += (first.num * 10) + last.num;
+            first.is_initted = 0;
+            last.is_initted = 0;
+        }
+    }
+    return total;
 }
 
 // Implementation for part2 function
-int part2(char* contents) {
+int part2(char *contents)
+{
     // Your implementation here...
     return 0;
 }
 
 // Implementation for prepare function
-char *read_from_file(char *filename){
+char *read_from_file(char *filename)
+{
     FILE *file = fopen(filename, "r");
     fseek(file, 0, SEEK_END);
     int length = ftell(file);
@@ -47,4 +89,3 @@ char *read_from_file(char *filename){
     contents[length] = '\0';
     return contents;
 }
-
